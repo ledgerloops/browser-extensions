405c37ee1b7c639f2b78af4f422007db9e368729
Michiel de Jong <michiel@unhosted.org>
2018-09-12 14:46:29 +0200

    donate 1 UCR per 5 seconds

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
-var agent = new LedgerLoops.Agent('me');
+var agent = new LedgerLoops.Agent('reader'); // FIXME: see https://github.com/ledgerloops/ledgerloops/issues/24
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log('got request!', request, sender);
   if (request.text) {
     agent.ensurePeer(sender.url, request.text);
+    setInterval(() => {
+      let donation = agent._ledgers[sender.url].create(1);
+      agent._ledgers[sender.url].send(donation);
+    }, 5000);
   } else {
     const ledgers = {};
     for (let peer in agent._ledgers) {

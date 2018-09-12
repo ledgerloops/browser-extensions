9889b8f9739eb8c77564ccffb824f56f301e03e2
Michiel de Jong <michiel@unhosted.org>
2018-09-12 13:32:41 +0200

    trying with onMessageExternal

--- background.js
+++ background.js
@@ -1,25 +1,3 @@
-var agent = new LedgerLoops.Agent('reader', true);
-console.log('agent created');
-
-function connect(bloggerSocket) {
-  console.log('agent created');
-  agent.ensurePeer('blogger', bloggerSocket)
-  console.log('peer ensured');
-  var donation = agent._ledgers.blogger.create(1);
-  console.log('donation created');
-  setTimeout(() => {
-    console.log('waited 1000ms');
-    agent._ledgers.blogger.send(donation);
-    console.log('donation sent');
-  }, 1000);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var port = chrome.tabs.connect(tabs[0].id);
-    port.onMessage.addListener(function getResp(response) {
-      console.log("resultsConnect", response);
-      connect(response);
-    });
-  });
+chrome.runtime.onMessageExternal.addListener(function(req, sender, sendRes) {
+  console.log('got msg, finally!', req);
 });
--- page.js
+++ page.js
@@ -1,18 +1,11 @@
 var elt = document.createElement('script');
-elt.innerHTML = 'window.setLedgerLoopsChannel = function(chan){' +
-                '  window.postMessage({ type: "LedgerLoops", text: chan }, "*");' +
-                '};' +
-                'console.log("LedgerLoops API loaded!");' +
-                'console.log("Try `window.setLedgerLoopsChannel(\'wss://unicurn.com/asdfasfrweds\');`");';
+elt.innerHTML = `\
+window.setLedgerLoopsChannel = function(chan){\
+  chrome.runtime.sendMessage('${chrome.runtime.id}', \
+    { type: "LedgerLoops", text: chan },\
+    function (response) { console.log('extension responded', response);\
+  });\
+};\
+console.log("LedgerLoops API loaded!");\
+console.log("Try window.setLedgerLoopsChannel('ws://localhost:8000');");`
 document.head.appendChild(elt);
-
-window.addEventListener("message", function(event) {
-  // We only accept messages from ourselves
-  if (event.source === window && event.data.type && event.data.type === 'LedgerLoops') {
-    console.log('LedgerLoops channel set, waiting for user to click the browser extension icon...');
-    chrome.runtime.onConnect.addListener(function(port) {
-      console.log('connected, setting channel!');
-      port.postMessage(event.data.text);
-    });
-  }
-}, false);

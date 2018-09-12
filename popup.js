830cbb5c5f0aff6b4b737397b3e4bd9e6002eb6b
Michiel de Jong <michiel@unhosted.org>
2018-09-12 11:24:08 +0200

    display ledgers

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
+var agent = new LedgerLoops.Agent('reader', true);
+
 function setChildTextNode(elementId, text) {
   document.getElementById(elementId).innerText = text;
 }
 
 function connect(bloggerSocket) {
-  var agent = new LedgerLoops.Agent('reader', true);
   console.log('agent created');
   agent.ensurePeer('blogger', bloggerSocket)
   console.log('peer ensured');
@@ -25,3 +26,45 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+function displayAgents() {
+  var html = '';
+  let loops = {};
+  html += `<p>Ledgers:</p><ul>`;
+  for (var neighbor in agent._ledgers) {
+    html += `<li>Ledger with ${neighbor}: ${agent._ledgers[neighbor].getBalance()}<ul>`;
+    let k;
+    for (k in agent._ledgers[neighbor]._committed) {
+      const entry = agent._ledgers[neighbor]._committed[k];
+      html += `<li><strong>Entry ${k}: ${entry.msgType} ${entry.beneficiary} ${entry.amount}</strong></li>`;
+      if (entry.msgType === 'COND') {
+        if (!loops[entry.routeId]) {
+          loops[entry.routeId] = {
+            start: entry.sender
+          };
+        }
+        loops[entry.routeId][entry.sender] = entry.beneficiary;
+      }
+    }
+    for (k in agent._ledgers[neighbor]._pending) {
+      const entry = agent._ledgers[neighbor]._pending[k];
+      html += `<li>(entry ${k}: ${entry.msgType} ${entry.beneficiary} ${entry.amount})</li>`;
+    }
+    html += '</ul></li>';
+  }
+  html += `</ul>`;
+  for (let routeId in loops) {
+    html += `<h2>Loop ${routeId}:</h2><p>`;
+    let cursor = loops[routeId].start;
+    do {
+      html += `-> ${cursor}`;
+      cursor = loops[routeId][cursor];
+      if (!cursor) {
+        break;
+      }
+    } while (cursor != loops[routeId].start);
+    html += '</p>';
+  }
+  document.getElementById('data').innerHTML = html;
+}
+setInterval(displayAgents, 1000);

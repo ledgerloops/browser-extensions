617ee0af92205c237fda3d16de4bb071c7bdf5af
Michiel de Jong <michiel@unhosted.org>
2018-09-17 11:33:02 +0200

    first stab at persistence, ref #7

--- background.js
+++ background.js
@@ -1,21 +1,37 @@
 var agent = new LedgerLoops.Agent('reader'); // FIXME: see https://github.com/ledgerloops/ledgerloops/issues/24
+browser.storage.local.get(['ledgers']).then(ledger => {
+  for (let peer in ledgers) {
+    console.log('inflating peer', ledger[peer]);
+    agent.ensurePeer(peer, ledgers[peer].channel);
+    agent._peerHandlers[peer]._ledger._committed = ledger[peer].committed;
+    agent._peerHandlers[peer]._ledger._pending = ledger[peer].pending;
+    agent._peerHandlers[peer]._ledger._currentBalance = ledger[peer].balanceDetails;
+  }
+});
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log('got request!', request, sender);
   if (request.text) {
     agent.ensurePeer(sender.url, request.text);
+    agent._peerHandlers[peer]._channel = request.text;
     setInterval(() => {
-      let donation = agent._ledgers[sender.url].create(1);
-      agent._ledgers[sender.url].send(donation);
+      let donation = agent._peerHandlers[sender.url].create(1);
+      agent._peerHandlers[sender.url].send(donation);
     }, 5000);
   } else {
     const ledgers = {};
-    for (let peer in agent._ledgers) {
+    for (let peer in agent._peerHandlers) {
       ledgers[peer] = {
-        balance: agent._ledgers[peer].getBalance(),
-        committed: agent._ledgers[peer]._committed,
-        pending: agent._ledgers[peer]._pending
+        channel: agent._peerHandlers[peer]._channel,
+        balance: agent._peerHandlers[peer].getBalance(),
+        balanceDetails: agent._peerHandlers[peer]._ledger._currentBalance,
+        committed: agent._peerHandlers[peer]._ledger._committed,
+        pending: agent._peerHandlers[peer]._ledger._pending
       };
     }
+    browser.storage.local.set({ ledgers }).then(() => {
+      console.log('ledgers stored!');
+    });
     sendResponse({ ledgers });
   }
 });

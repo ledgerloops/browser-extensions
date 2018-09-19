6fce066d872fb20afaa8835129ad5ff3249fd177
Michiel de Jong <michiel@unhosted.org>
2018-09-19 09:52:04 +0200

    remove flattrId

--- page.js
+++ page.js
@@ -2,8 +2,8 @@ console.log('waiting for dom to load!');
 window.onload = function () {
   console.log('dom loaded!');
   const ledgerloopsChannel = Array.from(document.getElementsByTagName('link')).filter(x => x.rel == 'ledger').map(x => x.href)[0]
-  console.log({ flattrId, ledgerloopsChannel });
-  if (flattrId) {
-    chrome.runtime.sendMessage({ flattrId, ledgerloopsChannel });
+  console.log({ ledgerloopsChannel });
+  if (ledgerloopsChannel) {
+    chrome.runtime.sendMessage({ ledgerloopsChannel });
   }
 };

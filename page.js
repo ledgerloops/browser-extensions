0425bd47543f3c0f1b67bcf3be9ed82fd6909e16
Michiel de Jong <michiel@unhosted.org>
2018-09-19 09:30:34 +0200

    change link-rel to just 'ledger'

--- page.js
+++ page.js
@@ -1,8 +1,7 @@
 console.log('waiting for dom to load!');
 window.onload = function () {
   console.log('dom loaded!');
-  const flattrId = Array.from(document.getElementsByTagName('meta')).filter(x => x.name == 'flattr:id').map(x => x.content)[0]
-  const ledgerloopsChannel = Array.from(document.getElementsByTagName('link')).filter(x => x.rel == 'ledgerloops:channel').map(x => x.href)[0]
+  const ledgerloopsChannel = Array.from(document.getElementsByTagName('link')).filter(x => x.rel == 'ledger').map(x => x.href)[0]
   console.log({ flattrId, ledgerloopsChannel });
   if (flattrId) {
     chrome.runtime.sendMessage({ flattrId, ledgerloopsChannel });
